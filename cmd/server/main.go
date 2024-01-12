b39a9d65f8868c89960c455eb2af908f195e2f73
kbondar17 <aa@bb.mail.ru>
2024-01-12 18:55:19 +0300

    debug host 2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,22 +1,19 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"metrics/internal/app"
-	"os"
 )
 
 func getConfig() string {
-	defaultHost := "127.0.0.1:8080"
-	if host, exists := os.LookupEnv("HOST"); exists {
-		defaultHost = host
-	}
+	// defaultHost := "localhost:8080"
+	// if host, exists := os.LookupEnv("HOST"); exists {
+	// 	defaultHost = host
+	// }
 
-	host := flag.String("host", defaultHost, "Адрес HTTP-сервера. По умолчанию localhost:8080")
-	flag.Parse()
-	fmt.Println("Сервер запущен на", *host)
-	return *host
+	// host := flag.String("host", defaultHost, "Адрес HTTP-сервера. По умолчанию localhost:8080")
+	// flag.Parse()
+	// return *host
+	return "http://localhost:8080"
 }
 
 func main() {
