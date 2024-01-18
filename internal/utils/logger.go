bd9aaf5a9e9c1604b1a590e6e4741e653958a0ba
kbondar17 <aa@bb.mail.ru>
2024-01-18 14:35:49 +0300

    uncomment logger

--- internal/utils/logger.go
+++ internal/utils/logger.go
@@ -1,29 +1,30 @@
 package utils
 
 import (
+	"io"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func NewLogger(filePath string, loggerPrefix string) *log.Logger {
 
-	// // Create log dir if not exists
-	// dir := filepath.Dir(filePath)
-	// if _, err := os.Stat(dir); os.IsNotExist(err) {
-	// 	err = os.MkdirAll(dir, 0755)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// }
-	// file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// Create log dir if not exists
+	dir := filepath.Dir(filePath)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// multi := io.MultiWriter(file, os.Stdout)
+	multi := io.MultiWriter(file, os.Stdout)
 
-	logger := log.New(os.Stdout, loggerPrefix, log.Ldate|log.Ltime)
-	// logger := log.New(multi, loggerPrefix, log.Ldate|log.Ltime)
+	logger := log.New(multi, loggerPrefix, log.Ldate|log.Ltime)
 
 	logger.Println("Logger initialized")
 
