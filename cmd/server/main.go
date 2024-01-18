d53360bc04b604b01171f63fb4550c050aee97a7
kbondar17 <aa@bb.mail.ru>
2024-01-18 10:28:12 +0300

    fix env 2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,27 +6,32 @@ import (
 	"os"
 )
 
-type ServerOptions struct {
-	EndpointAddr string `env:"ADDRESS"`
-}
+// type ServerOptions struct {
+// 	EndpointAddr string `env:"ADDRESS"`
+// }
 
-func (o *ServerOptions) ParseArgs() {
-	flag.StringVar(&o.EndpointAddr, "a", "localhost:8080", "endpoint address")
-	flag.Parse()
-}
+// func (o *ServerOptions) ParseArgs() {
+// 	flag.StringVar(&o.EndpointAddr, "a", "localhost:8080", "endpoint address")
+// 	flag.Parse()
+// }
 
-func (o *ServerOptions) ParseEnv() {
-	host := os.Getenv("ADDRESS")
-	if host != "" {
-		o.EndpointAddr = os.Getenv("ADDRESS")
-	}
-}
+// func (o *ServerOptions) ParseEnv() {
+// 	host := os.Getenv("ADDRESS")
+// 	if host != "" {
+// 		o.EndpointAddr = os.Getenv("ADDRESS")
+// 	}
+// }
+
+var Host string
 
 func getConfig() string {
-	opt := ServerOptions{}
-	opt.ParseArgs()
-	opt.ParseEnv()
-	return opt.EndpointAddr
+	flag.StringVar(&Host, "a", "localhost:8080", "endpoint address")
+	flag.Parse()
+
+	if ennvHost := os.Getenv("ADDRESS"); ennvHost != "" {
+		Host = ennvHost
+	}
+	return Host
 }
 
 func main() {
