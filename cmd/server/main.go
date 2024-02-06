b2c2aa4329ed9b27295005da26b8f26a796d018a
kbondar17 <aa@bb.mail.ru>
2024-02-06 19:24:56 +0300

    minor fixes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,10 +8,9 @@ func main() {
 	appConfig := app.NewAppConfigFromEnv()
 	app := app.NewApp(appConfig)
 
-	// если не синхронная запись, тогда запускаем фоновую задачу
-	if app.Config.StoreInterval > 0 {
+	if app.Config.StorageConfig.StoreInterval > 0 {
 		go func() {
-			app.SaveDataInInterval(app.Config.StoreInterval, app.Config.StoragePath)
+			app.SaveDataInInterval(app.Config.StorageConfig.StoreInterval, app.Config.StorageConfig.StoragePath)
 		}()
 	}
 
--- internal/agent/collector.go
+++ internal/agent/collector.go
@@ -43,6 +43,5 @@ func (coll *Collector) CollectMetrics(pollCount *int, container *m.MetricSendCon
 	*pollCount++
 
 	container.CounterMetrics["PollCount"] = fmt.Sprintf("%d", *pollCount)
-	// log.Println("Collected data: ", container)
 
 }
